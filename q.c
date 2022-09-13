2710d86fc2f5b08ee74ee66338b2208c1928a8a1
Timo3D <imeatingsoap@gmail.com>
2022-09-13 22:56:08 +0800

    hi

--- q.c
+++ q.c
@@ -1,8 +1,15 @@
-#include <stdio.h>
-
-int main(void) {
-    int x, y, z;
-    int r = scanf("%d-%d-%d", &x, &y, &z);
-    printf("%d\n", r);
-    return 0;
+void print_line(int index, int cents, int value)
+{
+// TODO: provide proper implementation later
+printf("print_line(%d, %d, %d)\n", index, cents, value);
+}
+void coins(int cents)
+{
+// TODO: provide proper implementation later
+printf("coins(%d)\n", cents);
+}
+int main(void)
+{
+// TODO: provide proper implementation later
+printf("main()\n");
 }
